e8061b69fc6c0ce5b474de45a8b1a0e1c2afd973
Akhmedov96 <ahmedovnahid@icloud.com>
2023-05-11 12:18:18 +0500

    Initial Commit

---
+++ Do homework RandomNumber/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Do homework RandomNumber
+//
+//  Created by Нахид  Ахмедов on 11/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
